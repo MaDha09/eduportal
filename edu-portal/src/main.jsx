7979a11e1ede86d814cbae06f006ff604726aa84
MaDha09 <dharelazura@gmail.com>
2025-04-17 11:24:54 +0800

    Setup of the website

--- edu-portal/src/main.jsx
+++ edu-portal/src/main.jsx
@@ -1,12 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import Dashboard from './main pages/Dashboard';
+import Login from './main pages/LogIn';
+import Profile from './main pages/Profile';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "/signup",
+    element: <Dashboard/>,
+  },
+  {
+    path: "/maindashboard",
+    element: <Profile/>,
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
